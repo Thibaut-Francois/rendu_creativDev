89bcd20fd4b081b9d5db75204493bc57093017ed
Vorn-Hagen <thibaut.francois@edu.devinci.fr>
2024-03-28 23:57:55 +0100

    :sparkles: Add romans letters

--- main.js
+++ main.js
@@ -18,25 +18,181 @@ ctx.closePath();
 
 ctx.beginPath();
 ctx.strokeStyle = 'lightcoral';
-ctx.moveTo(200, 350);
-ctx.lineTo(300, 350);
-ctx.lineTo(400, 350);
-ctx.lineTo(400, 400);
+ctx.moveTo(400, 150);
+ctx.lineTo(500, 150);
+ctx.lineTo(600, 150);
+ctx.lineTo(600, 200);
 ctx.stroke();
 
 ctx.closePath();
 
 ////////////////////////////////////////////////////////////////////////////
 
-function chiffre(nb, position){
-    ctx.save()
-    ctx.beginPath();
-    ctx.arc(position + 30, 0, 10, 0, deg2rad(360));
-    ctx.fillStyle = 'lightblue';
-    ctx.fill();
+function chiffre(nb, position, angle){
 
-    ctx.closePath();
-    ctx.restore()
+    if(nb==1){                          // 1
+        ctx.strokeStyle = 'lightcoral';
+        ctx.moveTo(position+30, 0);
+        ctx.lineTo(position+40, 20);
+        ctx.lineTo(position+50, 0);
+
+        ctx.moveTo(position+60, 0);
+        ctx.lineTo(position+60, 20);
+        ctx.stroke();
+
+    }else if(nb==2){                    // 2
+        ctx.strokeStyle = 'lightcoral';
+        ctx.moveTo(position+30, 0);
+        ctx.lineTo(position+40, 20);
+        ctx.lineTo(position+50, 0);
+
+        ctx.stroke();
+
+    }else if(nb==3){                          // 3
+        ctx.strokeStyle = 'lightcoral';
+        ctx.moveTo(position+60, 0);
+        ctx.lineTo(position+60, 20);
+
+        ctx.moveTo(position+30, 0);
+        ctx.lineTo(position+40, 20);
+        ctx.lineTo(position+50, 0);
+
+        ctx.stroke();
+
+    }else if(nb==4){                          // 4
+        ctx.strokeStyle = 'lightcoral';
+        ctx.moveTo(position+30, 0);
+        ctx.lineTo(position+30, 20);
+
+        ctx.moveTo(position+40, 0);
+        ctx.lineTo(position+40, 20);
+        
+        ctx.moveTo(position+50, 0);
+        ctx.lineTo(position+50, 20);
+
+        ctx.stroke();
+
+    }else if(nb==5){                          // 5
+        ctx.strokeStyle = 'lightcoral';
+        ctx.moveTo(position+30, 0);
+        ctx.lineTo(position+30, 20);
+
+        ctx.moveTo(position+40, 0);
+        ctx.lineTo(position+40, 20);
+
+        ctx.stroke()
+
+    }else if(nb==6){                          // 6
+        ctx.strokeStyle = 'lightcoral';
+        ctx.moveTo(position+30, 0);
+        ctx.lineTo(position+30, 20);
+
+        ctx.stroke();
+
+    }else if(nb==7){                        // 7
+        ctx.strokeStyle = 'lightcoral';
+        ctx.moveTo(position+30, 0);
+        ctx.lineTo(position+50, -20);
+        ctx.moveTo(position+50, 0);
+        ctx.lineTo(position+30, -20);
+
+        ctx.moveTo(position+60, 0);
+        ctx.lineTo(position+60, -20);
+
+        ctx.moveTo(position+70, 0);
+        ctx.lineTo(position+70, -20);
+
+        ctx.stroke();
+
+    }else if(nb==8){                          // 8
+        ctx.strokeStyle = 'lightcoral';
+        ctx.moveTo(position+50, 0);
+        ctx.lineTo(position+70, -20);
+        ctx.moveTo(position+70, 0);
+        ctx.lineTo(position+50, -20);
+
+        ctx.moveTo(position+30, 0);
+        ctx.lineTo(position+30, -20);
+
+        ctx.moveTo(position+40, 0);
+        ctx.lineTo(position+40, -20);
+
+        ctx.stroke();
+        
+    }else if(nb==9){                          // 9
+        ctx.strokeStyle = 'lightcoral';
+        ctx.moveTo(position+30, 0);
+        ctx.lineTo(position+50, -20);
+        ctx.moveTo(position+50, 0);
+        ctx.lineTo(position+30, -20);
+
+        ctx.stroke();
+        
+    }else if(nb==10){                          // 10
+        ctx.strokeStyle = 'lightcoral';
+        ctx.moveTo(position+30, 0);
+        ctx.lineTo(position+50, -20);
+        ctx.moveTo(position+50, 0);
+        ctx.lineTo(position+30, -20);
+
+        ctx.moveTo(position+60, 0);
+        ctx.lineTo(position+60, -20);
+
+        ctx.stroke();
+        
+    }else if(nb==11){                          // 11
+        ctx.strokeStyle = 'lightcoral';
+        ctx.moveTo(position+60, 0);
+        ctx.lineTo(position+70, -20);
+        ctx.lineTo(position+80, 0);
+
+        ctx.moveTo(position+30, 0);
+        ctx.lineTo(position+30, -20);
+
+        ctx.moveTo(position+40, 0);
+        ctx.lineTo(position+40, -20);
+
+        ctx.moveTo(position+50, 0);
+        ctx.lineTo(position+50, -20);
+
+        ctx.stroke();
+        
+        
+    }else if(nb==12){                          // 12
+        ctx.strokeStyle = 'lightcoral';
+        ctx.moveTo(position+50, 0);
+        ctx.lineTo(position+60, -20);
+        ctx.lineTo(position+70, 0);
+
+        ctx.moveTo(position+30, 0);
+        ctx.lineTo(position+30, -20);
+
+        ctx.moveTo(position+40, 0);
+        ctx.lineTo(position+40, -20);
+
+        ctx.stroke();
+        
+    }else if(nb==13){                          // 13
+        ctx.strokeStyle = 'lightcoral';
+        ctx.moveTo(position+40, 0);
+        ctx.lineTo(position+50, -20);
+        ctx.lineTo(position+60, 0);
+
+        ctx.moveTo(position+30, 0);
+        ctx.lineTo(position+30, -20);
+
+        ctx.stroke();
+        
+    }
+
+    // ctx.save()
+    // ctx.beginPath();
+    // ctx.arc(position + 30, 0, 10, 0, deg2rad(360));
+    // ctx.fillStyle = 'lightblue';
+    // ctx.fill();
+
+    // ctx.closePath();
+    // ctx.restore()
 }
 
 ///------------------------
@@ -44,7 +200,7 @@ function chiffre(nb, position){
 const nGraduation_ = 13
 ctx.strokeStyle = 'grey';
 test=0
-long=200
+long=300
 
 for(let i=0; i<nGraduation_; i++){
 //    console.log(i)
@@ -53,16 +209,26 @@ for(let i=0; i<nGraduation_; i++){
     console.log(angle_)
     ctx.save()
     ctx.beginPath()
-    ctx.translate(500, 500)
+    ctx.translate(400, 400)
     ctx.rotate(angle_)
-    ctx.moveTo(0, 0)
+    ctx.moveTo(25, 0)
     ctx.lineTo(long, 0)
     ctx.stroke()
 
-    chiffre(0, long)
-    
+
+
     ctx.closePath()
+
+
+    ctx.beginPath()
+    chiffre(i+1, long, angle_)
+    ctx.closePath()
+
+
     ctx.restore()
+
+
+
 }
 
 ///////////////////////////////////////////////////////////////////////////
